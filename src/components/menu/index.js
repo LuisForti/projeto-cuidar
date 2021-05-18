0bd48db2102cb2deb0abbd94e0719034d0b772a3
Lucas Coimbra <69476182+Nikodemos0612@users.noreply.github.com>
2021-05-18 07:53:31 -0300

    Feito o código inicial do menu e dos modals de criar conta e login

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,30 +1,96 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+import Modal from 'react-bootstrap/Modal';
+import ModalDialog from 'react-bootstrap/ModalDialog';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+
+import Form from 'react-bootstrap/Form'
 
 import Logo from '../../assets/images/Logo.png';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 
 import './Menu.css';
 
 export default class Menu extends Component{
 
-    render(){
-        return <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    ModalLogin(props) {
+        return(
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
 
-        <a class="navbar-brand"><img src="Logo.png" alt="Cuidar" id="logo"/></a>
-  
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-  
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item"> <a class="nav-link active" aria-current="page" href="#" id="btnLogin">Login</a></li>
-                <li class="nav-item"> <a class="nav-link active" aria-current="page" href="#" id="btnCriarConta">Cadastrar</a> </li>
-            </ul>
-  
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Login
+                    </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <Form>
+
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        )
+    }
+
+    ModalCriar(props){
+
+        return <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered>
+
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Criar Conta
+                </Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form>
+
+                </Form>
+            </Modal.Body>
+        </Modal>
+    }
+
+    Menu(){
+        const [modalLogin, setLoginShow] = React.useState(false);
+        const [modalCriar, setCriarShow] = React.useState(false);
+
+        return <div>
+
+            <ModalLogin show={modalLogin} onHide={() => setLoginShow(false)}/>
+            <ModalCriar show={modalCriar} onHide={() => setCriarShow(false)}/>
+
+            <Navbar expand="lg">
+                <Navbar.Brand>
+                    <Link to="/">
+                        <img src={Logo} alt="Cuidar" id="logo"/>
+                    </Link>
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+
+                <Navbar.Collapse>
+                    <Nav className="mr-auto">
+                        <Nav.Link onClick={() => setLoginShow(true)}>Login</Nav.Link>
+                        <Nav.Link onClick={() => setCriarShow(true)}>CriarConta</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
-      </nav>
+    }
+
+    render(){
+        return <Menu/>
     }
 }
